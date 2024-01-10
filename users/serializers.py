e34e42cea44aed188171489f8146572514f7ba88
kyeon06 <jkyeon06@gmail.com>
2024-01-11 02:16:49 +0900

    :sparkles: feat: 유저 로그인 구현

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 
+from django.contrib.auth import authenticate
+from django.utils import timezone
+
 from users.models import User
 
 
@@ -18,3 +21,32 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(username=username, password=password)
 
         return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=128, write_only=True)
+    last_login = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        username = data.get("username", None)
+        password = data.get("password", None)
+
+        if username is None:
+            raise serializers.ValidationError("username을 입력해주세요.")
+
+        if password is None:
+            raise serializers.ValidationError("password를 입력해주세요.")
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("해당 유저를 찾을 수 없습니다.")
+
+        if not user.is_active:
+            raise serializers.ValidationError("해당 유저는 비활성화되었습니다.")
+
+        user.last_login = timezone.now()
+        user.save(update_fields=["last_login"])
+
+        return {"username": user.username, "last_login": user.last_login}
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -5,7 +7,7 @@ from rest_framework.response import Response
 
 from drf_yasg.utils import swagger_auto_schema
 
-from users.serializers import UserRegisterSerializer
+from users.serializers import UserLoginSerializer, UserRegisterSerializer
 
 
 class UserRegisterAPIView(APIView):
@@ -32,7 +34,19 @@ class UserRegisterAPIView(APIView):
 
 
 class UserLoginAPIView(APIView):
-    pass
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(
+        operation_id="로그인",
+        operation_description="계정명, 비밀번호로 로그인합니다.",
+        request_body=UserLoginSerializer,
+        responses={200: "로그인 성공", 400: "로그인 실패"},
+    )
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserLogoutAPIView(APIView):
