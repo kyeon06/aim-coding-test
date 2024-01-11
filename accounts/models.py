1693a6c5909c2a2729441554fa625adcacdb0ac1
kyeon06 <jkyeon06@gmail.com>
2024-01-11 19:05:48 +0900

    :sparkles: feat: 계좌, 잔고 모델 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+# 계좌 정보 테이블
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.BigIntegerField(default=0)
+
+    class Meta:
+        db_table = "accounts"
+
+    def __str__(self):
+        return f"{self.user} : {self.balance}"
+
+
+# 잔고 입출력 정보 테이블
+class Balance(models.Model):
+    balance_date = models.DateTimeField(auto_now_add=True)
+    change_balance = models.BigIntegerField()
+    account = models.ForeignKey(
+        Account, related_name="balance_accounts", on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User, related_name="balance_users", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        db_table = "balances"
+
+    def __str__(self):
+        return f"{self.user} : {self.change_balance} : {self.balance_date}"
